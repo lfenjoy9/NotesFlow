a3883b584124899b417583bd20942ca92cb4dfa7
l9mtv <l9mtv@l9mtvs-MacBook-Pro-2.local>
2018-07-15 10:44:24 -0700

    Define the raw part.

--- server/hello.py
+++ server/hello.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('session.html', name='name')
 
 
 if __name__ == "__main__":
